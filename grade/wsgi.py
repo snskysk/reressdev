a5399d3a123faf1e06322c413b78d7c091fd4956
shunsuke <shun.210.suke@gmail.com>
2018-09-24 06:30:04 +0900

    make it better

--- grade/wsgi.py
+++ grade/wsgi.py
@@ -10,10 +10,15 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grade.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+
+#application = get_wsgi_application()
+
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(application)
