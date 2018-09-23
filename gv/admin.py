37f1569963b1898102806f12fa09ca6173ea1463
shunsuke <shun.210.suke@gmail.com>
2018-09-24 05:40:56 +0900

    make it better

--- gv/admin.py
+++ gv/admin.py
@@ -6,3 +6,9 @@ from .models import studentInfo, subjectInfo
 
 admin.site.register(studentInfo)
 admin.site.register(subjectInfo)
+
+
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
