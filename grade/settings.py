38103c4ab31e698d22cf356a59770921b5f0c98b
shunsuke <shun.210.suke@gmail.com>
2018-09-29 17:33:14 +0900

    first commit

--- grade/settings.py
+++ grade/settings.py
@@ -158,8 +158,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-#STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEBUG = False
 
