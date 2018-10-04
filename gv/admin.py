3b7581e7335e477686a14362093a69b4f277b26d
shunsuke <shun.210.suke@gmail.com>
2018-10-04 11:10:17 +0900

    first commit

--- gv/admin.py
+++ gv/admin.py
@@ -6,7 +6,7 @@ from .models import studentInfo, subjectInfo
 
 admin.site.register(studentInfo)
 admin.site.register(subjectInfo)
-
+admin.site.register(food_pool)
 
 from django.contrib import admin
 from .models import Post
