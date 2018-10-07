2f0bbde2d83ff75759c262b075fda340e2b4f781
shunsuke <shun.210.suke@gmail.com>
2018-10-07 19:07:06 +0900

    first commit

--- gv/models.py
+++ gv/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class studentInfo(models.Model):
@@ -37,25 +38,42 @@ class subjectInfo(models.Model):
 #################################################################
 
 class food_pool(models.Model):
+    five_list = [
+    (1,'1'),
+    (2,'2'),
+    (3,'3'),
+    (4,'4'),
+    (5,'5'),
+    ]
+    utilization_time_list = [['朝食','朝食'],['ブランチ','ブランチ'],['昼食','昼食'],['おやつ','おやつ'],\
+    ['夕食','夕食'],['夜食','夜食'],['飲み会','飲み会']]
+    sex_list = [['女','女'],['男','男'],['その他','その他']]
+    area_list = [['池袋駅・北','池袋駅・北'],['池袋駅・東','池袋駅・東'],['池袋駅・南','池袋駅・南'],['池袋駅・西','池袋駅・西']]
+    genre_list = [['和食','和食'],['洋食','洋食'],['中華料理','中華料理'],['ラーメン','ラーメン'],['カレー','カレー'],['焼肉','焼肉'],\
+    ['鍋','鍋'],['居酒屋・ダイニングバー','居酒屋・ダイニングバー'],['ファミレス','ファミレス'],]
+    price_list = [[100,'~200円'],[300,'201円~400円'],[500,'401円~600円'],[700,'601円~800円'],\
+    [900,'801円~1000円'],[1250,'1001円~1500円'],[1750,'1501円~2000円'],[2500,'2001円~3000円'],\
+    [3001,'3001円~'],[4001,'4001円~']]
+    number_list = [[1,'1人'],[2,'2人'],[3,'3人'],[4,'4人'],[5,'5人'],[6,'6人'],\
+    [10,'7人~13人'],[14,'14人~']]
+    duration_list = [[10,'~10分'],[15,'11分~20分'],[25,'21分~30分'],[30,'31分-60分'],[61,'1時間以上']]
+
     shop_name  = models.CharField(max_length = 30)
-    utilization_time = models.CharField(max_length = 30)
-    utilization_time_list = ['1','2']
-    area_list = [['西池袋','西池袋'],['東池袋','東池袋']]
+    utilization_time = models.CharField(max_length = 30, choices=utilization_time_list)
     area = models.CharField(max_length = 30, choices=area_list)
-    genre = models.CharField(max_length = 30)
-    price = models.IntegerField()
-    number_of_people = models.IntegerField()
-    evaluation_quality = models.IntegerField()
-    evaluation_amount = models.IntegerField()
-    duration = models.IntegerField()
-    sex = models.CharField(max_length = 30)
-    photograph_shine = models.IntegerField()
+    sex = models.CharField(max_length = 30, choices=sex_list)
+    genre = models.CharField(max_length = 30, choices=genre_list)
+    duration = models.IntegerField(choices=duration_list)
+    price = models.IntegerField(choices=price_list)
+    number_of_people = models.IntegerField(choices=number_list)
+    evaluation_quality = models.IntegerField(choices=five_list)
+    evaluation_amount = models.IntegerField(choices=five_list)
+    photograph_shine = models.IntegerField(choices=five_list)
     created_at = models.DateTimeField(auto_now_add=True)
-    '''
     def __str__(self):
         created_at = self.created_at
-        return created_at
-        '''
+        created_at = created_at + datetime.timedelta(hours=9)
+        return created_at.strftime('%Y/%m/%d %H:%M')+ self.shop_name
 
 ###########################################
 ###########################################
