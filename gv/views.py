dab0bf8daa1a457c0350307e560ba0db975d85a3
shunsuke <shun.210.suke@gmail.com>
2018-10-23 14:16:54 +0900

    first commit

--- gv/views.py
+++ gv/views.py
@@ -38,9 +38,6 @@ def exists_submit_token(request):
     submit_token = set_submit_token(request)
     return status
 
-###################################################################################
-                                    #トップページ
-######################################################################################
 
 
 ######################################################################################
@@ -126,10 +123,6 @@ def sirabasu(request):
     #return render(request, 'gv/teacher_search.html', sirabasu_params)
 
 
-"""
-def teacher_search(request):
-    return render(request, 'gv/informations.html')
-"""
 def teacher_search(request):
     sub_obj = subjectInfo.objects.all()
     teacher_list = list(set(list(subjectInfo.objects.values_list('teacher', flat=True))))#すべての先生のリスト(重複なし)
@@ -182,7 +175,9 @@ def teacher_search(request):
         'teacher_list':teacher_list,
     }
     return render(request, 'gv/teacher_search.html', teacher_search_params)
-
+###################################################################################
+                                    #トップページ
+######################################################################################
 
 def hp(request):
     request.session.flush()
@@ -194,11 +189,15 @@ def hp(request):
 
     return render(request, 'gv/hp.html', index_params)
 
+
+###################################################################################
+                                    #停止
+##################################################################################
+"""
 def info(request):
     return render(request, 'gv/informations.html')
-###################################################################################
-                                    #ログイン画面
-##################################################################################
+"""
+"""
 def index(request):
     form = userInfoForm()
     index_params = {
@@ -207,72 +206,8 @@ def index(request):
         }
 
     return render(request, 'gv/index.html', index_params)
-
-####################################################################################
-                                #ログイン後のメイン画面用
-####################################################################################
-def mainhome_after_login(request):
-    try: #ログインしているか確かめる
-        #mainhome_after_login = login
-        #print(mainhome_after_login)
-        mainhome_after_login_params = {}
-        str_name = request.session['str_name']
-        mainhome_after_login_params['str_name'] = str_name
-        gpa = request.session['gpa']
-        mainhome_after_login_params['gpa'] = gpa
-        #gradeAchievement = request.session['gradeAchievement']
-        #mainhome_after_login_params['gradeAchievement'] = gradeAchievement
-        kind_name = request.session['kind_name']
-        kind_name = kind_name[1:-1].replace(' ','').replace("'",'').split(',')
-        mainhome_after_login_params['kind_name'] = kind_name
-        Achivement_list = request.session['Achivement_list']
-        Achivement_list = Achivement_list[1:-1].replace(' ','').replace("'",'').split(',')
-        mainhome_after_login_params['Achivement_list'] = Achivement_list
-        unitOfcircle = request.session['unitOfcircle']
-        unitOfcircle = unitOfcircle[1:-1].replace(' ','').replace("'",'').split(',')
-        for i,j in enumerate(unitOfcircle):
-            num = int(unitOfcircle[i])
-            unitOfcircle[i] = num
-        mainhome_after_login_params['unitOfcircle'] = unitOfcircle
-        gradeOfcircle = request.session['gradeOfcircle']
-        gradeOfcircle = gradeOfcircle[1:-1].replace(' ','').replace("'",'').split(',')
-        mainhome_after_login_params['gradeOfcircle'] = gradeOfcircle
-        #residual_unit = request.session['residual_unit']
-        #residual_unit = residual_unit[1:-1].replace(' ','').replace("'",'').split(',')
-        #mainhome_after_login_params['residual_unit'] = residual_unit
-        #on_course = request.session['on_course']
-        #on_course = on_course[1:-1].replace(' ','').replace("'",'').split(',')
-        #mainhome_after_login_params['on_course'] = on_course
-
-        residual_num = request.session['residual_num']
-        mainhome_after_login_params['residual_num'] = residual_num
-        get_num = request.session['get_num']
-        mainhome_after_login_params['get_num'] = get_num
-        on_num = request.session['on_num']
-        mainhome_after_login_params['on_num'] = on_num
-
-        #棒グラフのsession
-        kind_name_bou = request.session['kind_name_bou']
-        mainhome_after_login_params['kind_name_bou'] = kind_name_bou
-        residual_unit_bou = request.session['residual_unit_bou']
-        mainhome_after_login_params['residual_unit_bou'] = residual_unit_bou
-        on_course_bou = request.session['on_course_bou']
-        mainhome_after_login_params['on_course_bou'] = on_course_bou
-
-        return render(request, 'gv/mainhome.html', mainhome_after_login_params)
-
-    except Exception:
-        mainhome_after_login = 'no'
-    #一度mainhomeに入っていれば実行
-    #if mainhome_after_login == 'ok':
-        #return render(request, 'gv/mainhome.html', mainhome_params)
-    #else:
-    form = userInfoForm()
-    index_params = {
-        'form':form
-    }
-    return render(request, 'gv/hp.html', index_params)
-
+"""
+"""
 def get(request):
     if request.method == 'POST':
         #formから学籍番号とパスワードの取得
@@ -349,104 +284,7 @@ def get(request):
         'message':'学籍番号とパスワードを入力しよう!'
         }
         return render(request, 'gv/index.html', params)
-###################################################################################
-
-######################################################################################
-
-
-######################################################################################
-                                     #詳細画面
-###################################################################################
-
-######################################################################################################
-                            #フォームを人それぞれで分ける 関数化
-######################################################################################################
-def make_list(model_name,form_name,sn,form):
-    kind_list = list(set(list(subjectInfo.objects.filter(user_id=sn).values_list('{}'.format(model_name), flat=True))))
-    the_field = form.fields['{}'.format(form_name)]
-    form_list = [('','{}'.format(form_name))]
-    for i in range(0, len(kind_list)):
-        form_tuple = (kind_list[i],kind_list[i])
-        form_list.append(form_tuple)
-        the_field.choices = form_list
-    return
-
-
-######################################################################################
-                                     #詳細画面
-###################################################################################
-
-def detail(request):
-    try:
-        sn=request.session['stunum']
-        #sn = personal_dataset.loc[0,'user_id']   #ログイン中のユーザーの学籍番号
-        #sn = '16cb087l'
-    #ログインしてからでなければ入れない
-    except Exception as e:
-        form = userInfoForm()
-        index_params = {
-            'form':form
-        }
-        return render(request, 'gv/hp.html', index_params)
-
-
-    if request.method == 'POST':
-        form = find_my_sub_Form(request.POST)
-        make_list('year_int','year',sn,form)
-        make_list('category1','category1',sn,form)
-        filtered_sub = subjectInfo.objects.filter(user_id=sn)
-        season = request.POST['season']
-        grade = request.POST['grade']
-        year = request.POST['year']
-        category1 = request.POST['category1']
-        subname_teacher = request.POST['subname_teacher']
-
-        #季節のフィルター
-        if len(season) is not 0:
-            filtered_sub = filtered_sub.filter(season__contains=season)
-        #成績のフィルター
-        if len(grade) is not 0:
-            g = int(grade)
-            filtered_sub = filtered_sub.filter(grade_score_int=g)
-        #科目名か先生の名前
-        if len(subname_teacher) is not 0:
-            filtered_sub = filtered_sub.filter(
-                Q(subjectname__contains=subname_teacher)|
-                Q(teacher__contains=subname_teacher)
-                ).distinct()
-        #category1のフィルター
-        if len(category1) is not 0:
-            filtered_sub = filtered_sub.filter(category1=category1)
-        #受講年度のフィルター
-        if len(year) is not 0:
-            y = int(year)
-            filtered_sub = filtered_sub.filter(year_int=y)
-
-        result_score_int_sum = filtered_sub.aggregate(Sum('result_score_int'))['result_score_int__sum']
-        unit_int_sum = filtered_sub.aggregate(Sum('unit_int'))['unit_int__sum']
-        gpa = result_score_int_sum / unit_int_sum
-
-        detail_params = {
-            'gpa_message':'該当科目のgpaは<b><u>'+str(round(gpa,2))+'</u></b><br>    ※Dがある場合正しく計算されません',
-            'filtered_sub':filtered_sub,
-            'form':form,
-        }
-        return render(request, 'gv/detail.html', detail_params)
-    form = find_my_sub_Form()
-    filtered_sub = subjectInfo.objects.filter(user_id=sn)
-    result_score_int_sum = filtered_sub.aggregate(Sum('result_score_int'))['result_score_int__sum']
-    unit_int_sum = filtered_sub.aggregate(Sum('unit_int'))['unit_int__sum']
-    gpa = result_score_int_sum / unit_int_sum
-    make_list('year_int','year',sn,form)
-    make_list('category1','category1',sn,form)
-    detail_params = {
-        'form':form,
-        'filtered_sub':filtered_sub
-    }
-    return render(request, 'gv/detail.html', detail_params)
-
-
-
+"""
 ###################################################################################
                                #メインのグラフを出力する画面
 #####################################################################################
@@ -599,10 +437,165 @@ def mainhome(request):
         }
         return render(request, 'gv/hp.html', index_params)
 
+####################################################################################
+                                #ログイン後のメイン画面用
+####################################################################################
+def mainhome_after_login(request):
+    try: #ログインしているか確かめる
+        #mainhome_after_login = login
+        #print(mainhome_after_login)
+        mainhome_after_login_params = {}
+        str_name = request.session['str_name']
+        mainhome_after_login_params['str_name'] = str_name
+        gpa = request.session['gpa']
+        mainhome_after_login_params['gpa'] = gpa
+        #gradeAchievement = request.session['gradeAchievement']
+        #mainhome_after_login_params['gradeAchievement'] = gradeAchievement
+        kind_name = request.session['kind_name']
+        kind_name = kind_name[1:-1].replace(' ','').replace("'",'').split(',')
+        mainhome_after_login_params['kind_name'] = kind_name
+        Achivement_list = request.session['Achivement_list']
+        Achivement_list = Achivement_list[1:-1].replace(' ','').replace("'",'').split(',')
+        mainhome_after_login_params['Achivement_list'] = Achivement_list
+        unitOfcircle = request.session['unitOfcircle']
+        unitOfcircle = unitOfcircle[1:-1].replace(' ','').replace("'",'').split(',')
+        for i,j in enumerate(unitOfcircle):
+            num = int(unitOfcircle[i])
+            unitOfcircle[i] = num
+        mainhome_after_login_params['unitOfcircle'] = unitOfcircle
+        gradeOfcircle = request.session['gradeOfcircle']
+        gradeOfcircle = gradeOfcircle[1:-1].replace(' ','').replace("'",'').split(',')
+        mainhome_after_login_params['gradeOfcircle'] = gradeOfcircle
+        #residual_unit = request.session['residual_unit']
+        #residual_unit = residual_unit[1:-1].replace(' ','').replace("'",'').split(',')
+        #mainhome_after_login_params['residual_unit'] = residual_unit
+        #on_course = request.session['on_course']
+        #on_course = on_course[1:-1].replace(' ','').replace("'",'').split(',')
+        #mainhome_after_login_params['on_course'] = on_course
+
+        residual_num = request.session['residual_num']
+        mainhome_after_login_params['residual_num'] = residual_num
+        get_num = request.session['get_num']
+        mainhome_after_login_params['get_num'] = get_num
+        on_num = request.session['on_num']
+        mainhome_after_login_params['on_num'] = on_num
+
+        #棒グラフのsession
+        kind_name_bou = request.session['kind_name_bou']
+        mainhome_after_login_params['kind_name_bou'] = kind_name_bou
+        residual_unit_bou = request.session['residual_unit_bou']
+        mainhome_after_login_params['residual_unit_bou'] = residual_unit_bou
+        on_course_bou = request.session['on_course_bou']
+        mainhome_after_login_params['on_course_bou'] = on_course_bou
+
+        return render(request, 'gv/mainhome.html', mainhome_after_login_params)
+
+    except Exception:
+        mainhome_after_login = 'no'
+    #一度mainhomeに入っていれば実行
+    #if mainhome_after_login == 'ok':
+        #return render(request, 'gv/mainhome.html', mainhome_params)
+    #else:
+    form = userInfoForm()
+    index_params = {
+        'form':form
+    }
+    return render(request, 'gv/hp.html', index_params)
+
+
+
+######################################################################################################
+                            #フォームを人それぞれで分ける 関数化
+######################################################################################################
+def make_list(model_name,form_name,sn,form):
+    kind_list = list(set(list(subjectInfo.objects.filter(user_id=sn).values_list('{}'.format(model_name), flat=True))))
+    the_field = form.fields['{}'.format(form_name)]
+    form_list = [('','{}'.format(form_name))]
+    for i in range(0, len(kind_list)):
+        form_tuple = (kind_list[i],kind_list[i])
+        form_list.append(form_tuple)
+        the_field.choices = form_list
+    return
+
+
+######################################################################################
+                                     #詳細画面
+###################################################################################
+
+def detail(request):
+    try:
+        sn=request.session['stunum']
+        #sn = personal_dataset.loc[0,'user_id']   #ログイン中のユーザーの学籍番号
+        #sn = '16cb087l'
+    #ログインしてからでなければ入れない
+    except Exception as e:
+        form = userInfoForm()
+        index_params = {
+            'form':form
+        }
+        return render(request, 'gv/hp.html', index_params)
+
+
+    if request.method == 'POST':
+        form = find_my_sub_Form(request.POST)
+        make_list('year_int','year',sn,form)
+        make_list('category1','category1',sn,form)
+        filtered_sub = subjectInfo.objects.filter(user_id=sn)
+        season = request.POST['season']
+        grade = request.POST['grade']
+        year = request.POST['year']
+        category1 = request.POST['category1']
+        subname_teacher = request.POST['subname_teacher']
+
+        #季節のフィルター
+        if len(season) is not 0:
+            filtered_sub = filtered_sub.filter(season__contains=season)
+        #成績のフィルター
+        if len(grade) is not 0:
+            g = int(grade)
+            filtered_sub = filtered_sub.filter(grade_score_int=g)
+        #科目名か先生の名前
+        if len(subname_teacher) is not 0:
+            filtered_sub = filtered_sub.filter(
+                Q(subjectname__contains=subname_teacher)|
+                Q(teacher__contains=subname_teacher)
+                ).distinct()
+        #category1のフィルター
+        if len(category1) is not 0:
+            filtered_sub = filtered_sub.filter(category1=category1)
+        #受講年度のフィルター
+        if len(year) is not 0:
+            y = int(year)
+            filtered_sub = filtered_sub.filter(year_int=y)
+
+        result_score_int_sum = filtered_sub.aggregate(Sum('result_score_int'))['result_score_int__sum']
+        unit_int_sum = filtered_sub.aggregate(Sum('unit_int'))['unit_int__sum']
+        gpa = result_score_int_sum / unit_int_sum
+
+        detail_params = {
+            'gpa_message':'該当科目のgpaは<b><u>'+str(round(gpa,2))+'</u></b><br>    ※Dがある場合正しく計算されません',
+            'filtered_sub':filtered_sub,
+            'form':form,
+        }
+        return render(request, 'gv/detail.html', detail_params)
+    form = find_my_sub_Form()
+    filtered_sub = subjectInfo.objects.filter(user_id=sn)
+    result_score_int_sum = filtered_sub.aggregate(Sum('result_score_int'))['result_score_int__sum']
+    unit_int_sum = filtered_sub.aggregate(Sum('unit_int'))['unit_int__sum']
+    gpa = result_score_int_sum / unit_int_sum
+    make_list('year_int','year',sn,form)
+    make_list('category1','category1',sn,form)
+    detail_params = {
+        'form':form,
+        'filtered_sub':filtered_sub
+    }
+    return render(request, 'gv/detail.html', detail_params)
+
+
 ######################################################################################
                                      #飲食店レビュー
 ###################################################################################
-
+"""
 def review(request):
     form = food_pool_Form()
     review_params = {
@@ -612,11 +605,11 @@ def review(request):
         form = food_pool_Form(request.POST)
         form.save()
     return render(request, 'gv/review.html', review_params)
-
+"""
 ######################################################################################
                                      #飲食店検索
 ###################################################################################
-
+"""
 def shop_search(request):
     if request.method == 'POST':
         form = find_shop(request.POST)
@@ -642,3 +635,4 @@ def shop_search(request):
         'shop_obj':shop_obj
     }
     return render(request, 'gv/shop_search.html', shop_search_params)
+"""
