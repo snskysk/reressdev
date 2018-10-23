a8f2d186fcd2f6e6fd8d0d5271ec50136251e59b
shunsuke <shun.210.suke@gmail.com>
2018-10-23 13:47:41 +0900

    first commit

---
+++ gv/static/gv/homepage/purogres.js
@@ -0,0 +1,10 @@
+var bar = new ProgressBar.Line(container, {
+strokeWidth: 4,
+easing: 'easeInOut',
+duration: 1400,
+color: '#A9BCF5',
+trailColor: '#eee',
+trailWidth: 1,
+svgStyle: {width: '100%', height: '100%'}
+});
+bar.animate(1.0);
